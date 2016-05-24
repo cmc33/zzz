6514c08cba979e2a462db46bb03bdb75e18c7b26
cmc33 <cmcguigan33@gmail.com>
2016-05-24 19:57:46 -0400

    woo?

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,14 @@
 def fizzbuzz(num)
   if num % 5 == 0 && num % 3 == 0
     return "Fizzbuzz"
+  elsif num % 3 != 0 and num % 5 != 0
+    return "WompWomp"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+    
+  end
+end
+
+
